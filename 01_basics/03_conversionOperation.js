360ddf6805a11a4464cc07ec9256a0a6b9357e37
Yash-Y09 <kingmekar307@gmail.com>
2024-03-13 22:28:33 +0530

    learn Operations and write

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,7 +12,7 @@ let nameq = "king"
  let isLoggedIn = 0
 
  let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn) // O/P: false
+//  console.log(booleanIsLoggedIn) // O/P: false
  // after convert in boolean
 //  "hitesh" => true
 // "" => false
@@ -23,3 +23,46 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber) // O/P: 33
 console.log(typeof stringNumber) // O/P: string
+// ****************** Operations *************************
+
+let value = 3
+let negValue = -value
+console.log(negValue) // O/P: -3
+
+console.log(2+2);// O/P:4
+console.log(2-2);// O/P:0
+console.log(2*2);// O/P:4
+console.log(2**3);// O/P:8
+console.log(2/3);// O/P: 0.666666666666
+console.log(2%3);// O/P: 2
+
+let str1 = "hello"
+let str2 = " Monarch"
+
+let str3 = str1 + str2
+console.log(str3)// O/P: hello Monarch
+
+console.log("1" + 2)// O/P: 12
+console.log("1" + "2")// O/P: 12 
+console.log(1 + "2")// O/P: 12
+console.log(1 + 2 + "2")// O/P: 32
+console.log("1" + 2 + 2)// O/P: 122
+
+
+console.log(true); // O/P: true
+console.log(+true); // O/P: 1
+console.log(true+); // O/P: error
+console.log(+""); // O/P: 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)// O/P: 101
+
+let gameCounter1 = 100
+++gameCounter1;
+console.log(gameCounter1)// O/P: 101
+
