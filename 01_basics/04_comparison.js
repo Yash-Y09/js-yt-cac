41dcbd320d8bb204f0bf7d9f2176897299fef989
kingmekar307@gmail.com <kingmekar307@gmail.com>
2024-03-14 11:57:54 +0530

    Comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2<1); //O/P: false
+// console.log(2<=1); //O/P: false
+// console.log(2>1); //O/P: true
+// console.log(2>=1); //O/P: true
+// console.log(2!=1); //O/P: true
+
+// console.log(2==1); //O/P: false
+
+// console.log("2">1);  //O/P: true
+// console.log("0" > 1);  //O/P: false
+
+// console.log(null > 0) //O/P: false
+// console.log(null < 0) //O/P: false
+// console.log(null == 0) //O/P: false
+// console.log(null >= 0) //O/P: true
+// console.log(null <= 0) //O/P: true
+
+
+console.log(undefined == "a") //O/P: false
+console.log(undefined < 0) //O/P: false
+console.log(undefined > 0) //O/P: false
+console.log(undefined == 0) //O/P: false
+
+
+//===
+
+console.log("2"===2) //O/P: false
