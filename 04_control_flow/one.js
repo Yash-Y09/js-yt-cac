2b625d64964d964f28f3051f19ebeeff0112b654
Yash Prajapati <kingmekar307@gmail.com>
2024-03-29 20:43:52 +0530

    04 flw

---
+++ 04_control_flow/one.js
@@ -0,0 +1,17 @@
+// if 
+// if(condition){
+// 
+// } // condition true then only code run which is insie of { }
+
+// if(false){} // it will never run code because condition is false already.
+
+const isUserLoggedIn = true
+
+if (isUserLoggedIn) {
+    console.log(`welcome`);
+}
+
+// Comparision Oprator
+// <, >, <=, >=, ==, !=, ===, !==
+
+// && -> and, || -> or, 
